2df0e14d1aa36a5b1a5c2609cd98de4843f10c83
MouadLahlal <76909759+MouadLahlal@users.noreply.github.com>
2024-04-15 20:24:09 +0200

    resolved the issue "Download error : Forbidden"

--- index.js
+++ index.js
@@ -1,59 +1,70 @@
-import yargs from 'yargs';
-import fetch from "node-fetch";
-import { writeFileSync } from "fs";
-import PromptSync from 'prompt-sync';
+import sqlite3 from "sqlite3";
+import AdmZip from "adm-zip";
+import PDFMerger from "pdf-merger-js";
+import request from "sync-request";
+import fsExtra from "fs-extra";
+import yargs from "yargs";
+import PromptSync from "prompt-sync";
+
+let data;
+let volumeId;
+let token;
+let platform;
+let title;
 
 const prompt = PromptSync({ sigint: true });
 
 const argv = yargs(process.argv)
-  .option('platform', {
-	alias: 'p',
-	description: 'Platform to download from, either "hubyoung" or "hubkids"',
-	type: 'string',
-  })
-  .option('volumeId', {
-    alias: 'v',
-    description: 'Volume ID of the book to download',
-    type: 'string',
-  })
-  .option('container', {
-    alias: 'c',
-    description: 'Container for the book (either Kids or Young)',
-    type: 'string',
-  })
-  .option('token', {
-    alias: 't',
-    description: 'Token of the user',
-    type: 'string',
-  })
-  .option('file', {
-    alias: 'f',
-    description: 'The output file (defaults to book name)',
-    type: 'string',
-  })
-  .help()
-  .alias('help', 'h').argv;
+    .option("platform", {
+        alias: "p",
+        description:
+            'Platform to download from, either "hubyoung" or "hubkids"',
+        type: "string",
+    })
+    .option("volumeId", {
+        alias: "v",
+        description: "Volume ID of the book to download",
+        type: "string",
+    })
+    .option("container", {
+        alias: "c",
+        description: "Container for the book (either Kids or Young)",
+        type: "string",
+    })
+    .option("token", {
+        alias: "t",
+        description: "Token of the user",
+        type: "string",
+    })
+    .option("file", {
+        alias: "f",
+        description: "The output file (defaults to book name)",
+        type: "string",
+    })
+    .help()
+    .alias("help", "h").argv;
 
-(async () => {
+async function initialize() {
+    volumeId = argv.volumeId;
+    token = argv.token;
+    platform = argv.platform;
 
-    let volumeId = argv.volumeId;
-    let token = argv.token;
-	let platform = argv.platform;
+    fsExtra.mkdirSync("temp");
 
-	while (!platform) {
-		platform = prompt("Input the platform (either 'hubyoung' or 'hubkids'): ");
-		if (platform !== 'hubyoung' && platform !== 'hubkids') {
-			console.log("Invalid platform, please input either 'hubyoung' or 'hubkids'");
-			platform = null;
-		}
-	}
-	platform = platform === 'hubyoung' ? 'young' : 'kids';
-    while (!volumeId)
-        volumeId = prompt("Input the volume ID: ");
-    while (!token)
-        token = prompt("Input the token: ");
-
-    console.log("Obtaining volume info . . . ");
+    while (!platform) {
+        platform = prompt(
+            "Input the platform (either 'hubyoung' or 'hubkids'): "
+        );
+        if (platform !== "hubyoung" && platform !== "hubkids") {
+            console.log(
+                "Invalid platform, please input either 'hubyoung' or 'hubkids'"
+            );
+            platform = null;
+        }
+    }
+    platform = platform === "hubyoung" ? "young" : "kids";
+    while (!volumeId) volumeId = prompt("Input the volume ID: ");
+    while (!token) token = prompt("Input the token: ");
 
     let response = await fetch("https://ms-api.hubscuola.it/me" + platform + "/publication/" + volumeId, { method: "GET", headers: { "Token-Session": token, "Content-Type": "application/json" } });
     const code = response.status;
@@ -63,19 +74,107 @@ const argv = yargs(process.argv)
         console.log("Token Session not valid, you may have copied it wrong or you don't own this book.");
     } else {
         let result = await response.json();
-        const title = result.title;
-        console.log(`Downloading "${title}"\nObtaining access token . . .`);
-        let auth = await fetch(`https://ms-pdf.hubscuola.it/i/d/${volumeId}/auth`, { 
-            method: "POST", 
-            body: JSON.stringify({jwt: result.jwt, origin: `https://${platform}.hubscuola.it/viewer/${volumeId}?page=1`}), 
-            headers: { 
-                "PSPDFKit-Platform": "web", 
-                "PSPDFKit-Version": "protocol=3, client=2020.6.0, client-git=63c8a36705"
-            } 
-        }).then(res => res.json());
-        console.log(`Downloading PDF (may take a couple of seconds) . . .`);
-        let pdf = await fetch(`https://ms-pdf.hubscuola.it/i/d/${volumeId}/h/${auth.layerHandle}/pdf?token=${auth.token}`).then(res => res.arrayBuffer());
-        writeFileSync(argv.file || (title + '.pdf'), Buffer.from(pdf));
-        console.log(`Saving!`);
+        title = result.title;
+        console.log(`Downloading "${title}"...`);
     }
-})();
+}
+
+async function downloadZip() {
+    const zipFilePath = "temp/data.zip";
+    return new Promise(async (resolve, reject) => {
+        var res = request(
+            "GET",
+            `https://ms-mms.hubscuola.it/downloadPackage/${volumeId}/publication.zip?tokenId=${token}`,
+            { headers: { "Token-Session": token } }
+        );
+        if (res.statusCode !== 200) {
+            console.error("Errore API:", res.statusCode);
+            resolve();
+        }
+        await fsExtra.writeFile(zipFilePath, res.body, (err) => {
+            if (err) {
+                console.error(err);
+                resolve();
+            }
+
+            console.log("Downloaded chapters...");
+            resolve();
+        });
+    });
+}
+
+async function extractZip() {
+    const zipFilePath = "temp/data.zip";
+    const extractDir = "temp/extracted-files";
+    const zip = new AdmZip(zipFilePath);
+
+    zip.extractAllTo(extractDir);
+
+    console.log("Extracted chapters...");
+}
+
+async function connectDb() {
+    return new Promise((resolve, rejects) => {
+        let db = new sqlite3.Database(
+            "./temp/extracted-files/publication/publication.db",
+            (err) => {
+                if (err) {
+                    console.error(err.message);
+                }
+            }
+        );
+        db.get(
+            "SELECT offline_value FROM offline_tbl WHERE offline_path=?",
+            [`meyoung/publication/${volumeId}`],
+            (err, row) => {
+                if (err) {
+                    console.error(err);
+                    resolve();
+                }
+                if (!row) {
+                    resolve();
+                }
+                data = JSON.parse(row.offline_value).indexContents.chapters;
+                console.log("Fetched chapters");
+                resolve();
+            }
+        );
+        db.close();
+    });
+}
+
+async function extractPages() {
+    for (const chapter of data) {
+        const url = `https://ms-mms.hubscuola.it/public/${volumeId}/${chapter.chapterId}.zip?tokenId=${token}&app=v2`;
+        var res = await fetch(url, {
+            headers: { "Token-Session": token },
+        }).then((res) => res.arrayBuffer());
+        const zip = new AdmZip(Buffer.from(res));
+        await zip.extractAllTo(`temp/build`);
+    }
+    console.log("Extracted pages...");
+}
+
+async function mergePages() {
+    const merger = new PDFMerger();
+    console.log("Merging pages...");
+    for (const chapter of data) {
+        let base = `./temp/build/${chapter.chapterId}`;
+        const files = fsExtra.readdirSync(base);
+        for (const file of files) {
+            if (file.includes(".pdf")) {
+                await merger.add(`${base}/${file}`);
+            }
+        }
+    }
+    merger.save(`${title}.pdf`);
+    fsExtra.removeSync("temp");
+    console.log("Book saved");
+}
+
+await initialize();
+await downloadZip();
+extractZip();
+await connectDb();
+await extractPages();
+mergePages();
